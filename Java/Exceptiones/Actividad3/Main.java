5203cf9ad593ffcae3920e411a3caf0d6073a393
ErosMZ <151672271+ErosMZ@users.noreply.github.com>
2024-02-27 20:19:27 +0100

    Update Main.java

--- Java/Exceptiones/Actividad3/Main.java
+++ Java/Exceptiones/Actividad3/Main.java
@@ -1,19 +1,24 @@
-package Exceptiones.Actividad3;
+package Actividad3;
 
 public class Main{
     
-    @SuppressWarnings("removal")
     public static void main(String[] args) {
 
-        Pieza p1 = new Pieza("cuadrado","rojo");
-        Pieza p2 = new Pieza("cuadrado","rojo");
-        Double d = new Double(1.0);
-        String k = "Hola";
-        boolean b1 = p1.equals(p2);
-        boolean b2 = d.equals(k);
-        boolean b3 = k.equals(p2);
-        boolean b4 = p1.equals(d);
-        
+        try{
+
+            Pieza p1 = new Pieza("cuadrado","rojo");
+            Pieza p2 = new Pieza("cuadrado","rojo");
+            Double d = new Double(1.0);
+            String k = "Hola";
+            boolean b1 = p1.equals(p2);
+            boolean b2 = d.equals(k);
+            boolean b3 = k.equals(p2);
+            boolean b4 = p1.equals(d);
+        }catch(ClassCastException eros){
+            
+            System.out.println("Clases incomparables");
+
+        }  
     }
 
 }
