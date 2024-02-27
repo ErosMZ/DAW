d1b5066be69ef42a64f1897e740e4755a7553493
ErosMZ <151672271+ErosMZ@users.noreply.github.com>
2024-02-27 20:19:54 +0100

    Update Pieza.java

--- Java/Exceptiones/Actividad3/Pieza.java
+++ Java/Exceptiones/Actividad3/Pieza.java
@@ -1,27 +1,26 @@
-package Exceptiones.Actividad3;
+package Actividad3;
 
 public class Pieza {
-    
-    private String forma = "";
-    private String color = "";
-    private Object nombre;
 
-    public Pieza(String string, String string2) {
-        this.forma = string;
-        this.color = string2;
+    private String nombre = "";
+    private String color = "";
+
+    public Pieza (String nombre , String color){
+
+        
     }
 
-    public boolean equals(Object o){
-
+    public double area() {
+        return 1;
+    }
+    
+    public boolean equals(Object o) {
+        
         Pieza p = (Pieza) o;
         return this.color.equals(p.color) &&
-            this.nombre.equals(p.nombre) &&
-            this.area() == p.area();
+        this.nombre.equals(p.nombre) &&
+        this.area() == p.area();
 
-    }
-
-    private Object area() {
-        throw new UnsupportedOperationException("Juan");
-    }
+        }
 
 }
