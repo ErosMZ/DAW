3ee09f810714df234683160c6504ea718d5ec0e0
ErosMZ <151672271+ErosMZ@users.noreply.github.com>
2024-03-21 19:41:47 +0100

    Delete Sistemas/Ev3/bash directory

--- Sistemas/Ev3/bash/actividades/Actividad1/donde_esta.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-for i in `echo $PATH | tr ":" " "`
-do
-	if [ -x $i/$1  ]; then
-		echo localizado en $i
-		whatis $1
-#		man -f $1
-	fi
-done
--- Sistemas/Ev3/bash/actividades/Actividad1/lista_fich.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-for fichero in  $*
-do
-	echo "El contendido de  $fichero es: "
-	cat $1
-	echo
-done
