bee7b0cc80a6460c83955ab3644cfb0c7900224c
ErosMZ <151672271+ErosMZ@users.noreply.github.com>
2024-03-25 17:53:29 +0100

    Delete Sistemas/Ev3/bash/actividades directory

--- Sistemas/Ev3/bash/actividades/Actividad1/quienesta.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-echo Hay  `who | wc -l` usuarios del sistema
-# wc sirve para contar cosas y el -l sirve solo para las lineas
-echo Hoy es: `date`
--- Sistemas/Ev3/bash/actividades/Actividad2/nusergroup.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-cut -d ":" -f4 /etc/passwd | sort -n | uniq -c | sort -nr
