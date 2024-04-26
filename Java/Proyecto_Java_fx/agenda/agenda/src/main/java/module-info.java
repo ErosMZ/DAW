83b946153751fc7f387ce660c43aefcf6618ebe5
ErosMZ <151672271+ErosMZ@users.noreply.github.com>
2024-04-26 19:00:40 +0200

    Delete Java/Proyecto_Java_fx directory

--- Java/Proyecto_Java_fx/agenda/agenda/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module agenda {
-    requires javafx.controls;
-    requires javafx.fxml;
-    requires javafx.base;
-
-    opens agenda to javafx.fxml;
-    exports agenda;
-}
--- Java/Proyecto_Java_fx/demo/src/main/java/module-info.java
+++
@@ -1,7 +0,0 @@
-module eros_fx {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-    opens eros_fx to javafx.fxml;
-    exports eros_fx;
-}
