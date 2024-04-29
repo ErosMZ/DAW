36c7d36932379ef4c9d9c66bdfa79cb7f155200e
ErosMZ <151672271+ErosMZ@users.noreply.github.com>
2024-04-29 20:32:07 +0200

    Add files via upload

---
+++ entornos/testing/avengers/src/test/java/org/antonio/TestHeroes.java
@@ -0,0 +1,82 @@
+package org.antonio;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+import org.antonio.Exception.HeroeNoEncontradoException;
+import org.antonio.Model.GestorHeroes;
+import org.antonio.Model.Heroe;
+import org.junit.Before;
+
+public class TestHeroes {
+
+    Heroe ironman = null;
+    Heroe spiderman= null;
+    Heroe capitanAmerica= null;
+
+    GestorHeroes gh = new GestorHeroes();
+    @Before
+    public void setUp(){
+
+        ironman= new Heroe("Iron Man", "Traje de alta tecnología", "Millonario y filántropo");
+        spiderman=  new Heroe("Spider-Man", "Sentido arácnido, trepador", "Tímido estudiante de secundaria");
+        capitanAmerica = new Heroe("Capitán América", "Superfuerza, agilidad, resistencia", "Soldado de la Segunda Guerra Mundial");
+        
+        gh.agregarHeroe(ironman);
+        gh.agregarHeroe(spiderman);
+        gh.agregarHeroe(capitanAmerica);
+    
+    }
+
+    @Test
+    public void testGettersHeroe(){
+
+        assertEquals("Iron Man", ironman.getNombre());
+        assertEquals("Traje de alta tecnología", ironman.getSuperpoderes());
+        assertEquals("Millonario y filántropo", ironman.getBiografia());
+
+        assertNotEquals("Ant Man", spiderman.getNombre());
+        assertNotEquals("Superfuerza, agilidad", capitanAmerica.getSuperpoderes());
+        assertNotEquals("Soldado de la Segunda Guerra Mundial", ironman.getBiografia());
+    }
+
+    @Test
+    public void testSettersHeroe() {
+
+        Heroe catwoman = new Heroe(null, null, null);
+
+        catwoman.setNombre("Catwoman");
+        catwoman.setSuperpoderes("uwu");
+        catwoman.setBiografia("uwu");
+
+        assertEquals("Catwoman", catwoman.getNombre() );
+        assertEquals("uwu", catwoman.getSuperpoderes());
+        assertEquals("uwu", catwoman.getBiografia());
+    }
+
+ 
+
+    @Test
+    public void testBuscarHeroe() throws HeroeNoEncontradoException{ 
+
+        assertEquals(ironman, gh.buscarHeroe("Iron Man"));
+        assertEquals(capitanAmerica, gh.buscarHeroe("Capitán América"));
+        assertNotEquals(spiderman, gh.buscarHeroe("Iron Man"));
+
+        // en este no he usado el buscarHeroe porque si pongo uno que no está salta la excepción 
+        assertNotEquals(spiderman, "Paco");
+
+
+    }
+
+    @Test(expected = HeroeNoEncontradoException.class)
+    public void testExceptionBuscarHeroe() throws HeroeNoEncontradoException{
+        
+        gh.buscarHeroe("Juan");
+
+    }
+    
+}
+
+
